6b5ceb581907d11757b36d92f0492b20c642ccc4
Wakefield <jsw5227@live.missouristate.edu>
2019-04-15 13:11:04 -0500

    add infinite scroll

--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@ import { $ } from "./utils/dom.js";
 import "./utils/template.js";
 import "./main.css";
 
+let numElements = 0;
+
 (() => {
 	$(document).ready((e) => {
-		for(let i =0; i < 100; i++){
+		for(let i = 0; i < 10; i++){
 			$("#mainContent").template("hello",{ 
 				title: "title"+i,
 				text: "this is some text for card: "+i 
 			}).append();
 		}
+		numElements += 10;
+		window.addEventListener("scroll", (e) => {
+			if(window.scrollY >= document.body.scrollHeight - 1000) {
+				for(let i = numElements; i < numElements+10; i++){
+					$("#mainContent").template("hello",{ 
+						title: "title"+i,
+						text: "this is some text for card: "+i 
+					}).append();
+				}
+				numElements += 10;
+			}
+			
+		});
 	});
 })();
